120176083e4876e1c561fb7c9703b139fa0b9e85
Anferne San Juan <sanjuananferne@gmail.com>
2024-10-23 19:38:00 +0800

    quevent spike skeleton with graphql

---
+++ src/main/java/com/api/quevent_spike/QueventSpikeApplication.java
@@ -0,0 +1,13 @@
+package com.api.quevent_spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueventSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueventSpikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/quevent_spike/QueventSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.quevent_spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueventSpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
